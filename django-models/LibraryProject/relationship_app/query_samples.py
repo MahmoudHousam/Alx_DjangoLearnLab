c94cf74d356f78be3fd6ac05d2b672ea21cff24b
MahmoudHousam <mahmoudhousam60@gmail.com>
2024-08-18 23:26:10 +0300

    task 1

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,43 @@
+from .models import Author, Book, Library, Librarian
+
+# Add Author objects
+nageeb = Author.objects.create(name="Nageeb Mahfouz")
+nageeb.save()
+ehsan = Author.objects.create(name="Ehsan Abdelkkodos")
+ehsan.save()
+author_name = Author.objects.create(name="Author Name")
+
+# Add Book objects
+cairo_30 = Book.objects.create(title="Cairo 30", author=nageeb)
+arabisk = Book.objects.create(title="Arabisk", author=ehsan)
+
+cairo_30.save()
+arabisk.save()
+
+# Get books by one author
+Book.objects.get(author=ehsan)
+Book.objects.get(author=nageeb)
+Author.objects.get(name=author_name)
+Author.objects.filter(author=author_name)
+
+# List all books
+Book.objects.all()
+
+# Create Library Instance
+grow = Library.objects.create(name="grow")
+grow.save()
+
+# Add books to Library
+grow.books.add(cairo_30, arabisk)
+
+# List all books in Library
+grow.books.all()
+# Library.objects.get(name=library_name)
+
+# Add Librarian
+agyim = Librarian.objects.create(name="Agyim Taala", library=grow)
+agyim.save()
+
+# Retrieve a Libryrian for a library
+agyim.library
+Librarian.objects.get(library=grow)
