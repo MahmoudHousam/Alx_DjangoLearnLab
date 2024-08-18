4065cbbf6bfb0c4c7ad3d8296c53b049c7b99776
MahmoudHousam <mahmoudhousam60@gmail.com>
2024-08-18 23:32:28 +0300

    task 1

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -5,17 +5,29 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField()
 
+    def __str__(self):
+        return self.name
+
 
 class Book(models.Model):
     title = models.CharField()
     author = models.ForeignKey(Author)
+
+    def __str__(self):
+        return self.name
 
 
 class Library(models.Model):
     name = models.CharField()
     books = models.ManyToManyField(Book)
 
+    def __str__(self):
+        return self.name
+
 
 class Librarian(models.Model):
     name = models.CharField()
     library = models.OneToOneField(Library)
+
+    def __str__(self):
+        return self.name
